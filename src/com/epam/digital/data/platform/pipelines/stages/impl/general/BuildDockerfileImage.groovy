086d254a948de646add130cc0aacee66b616c9df
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2021-09-29 17:12:24 +0000

    [EDP-DDM-7683] Added fix for rendering dockerfiles
    Change-Id: I3c6c4614a2de1d61939445519c06e250687145d7

--- src/com/epam/digital/data/platform/pipelines/stages/impl/general/BuildDockerfileImage.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/general/BuildDockerfileImage.groovy
@@ -3,6 +3,7 @@ package com.epam.digital.data.platform.pipelines.stages.impl.general
 import com.epam.digital.data.platform.pipelines.buildcontext.BuildContext
 import com.epam.digital.data.platform.pipelines.stages.ProjectType
 import com.epam.digital.data.platform.pipelines.stages.Stage
+import com.epam.digital.data.platform.pipelines.tools.TemplateRenderer
 
 @Stage(name = "build-image-from-dockerfile", buildTool = ["any"], type = [ProjectType.APPLICATION])
 class BuildDockerfileImage {
@@ -12,22 +13,27 @@ class BuildDockerfileImage {
 
     void run() {
         if (context.script.fileExists("${context.workDir}/Dockerfile")) {
-            String dockerfileString = context.script.readFile file: "${context.workDir}/Dockerfile"
-            dockerfileString = dockerfileString.replaceAll(/FROM (.*)/, "FROM ${context.dockerRegistry.proxyHost}/" + '\$1')
-            context.script.writeFile(file: "${context.workDir}/Dockerfile", text: dockerfileString)
+            String globalNexusNamespace = context.dnsWildcard.startsWith("apps.cicd") ?
+                    'mdtu-ddm-edp-cicd' : 'control-plane'
+            String dockerFileSourcePath = "${context.workDir}/Dockerfile"
+            String dockerFileDestPath = "${context.workDir}/Dockerfile"
+            updateDockerfile(dockerFileSourcePath, context.dockerRegistry.proxyHost, globalNexusNamespace,
+                    dockerFileDestPath)
         } else {
-            context.script.error "There is no Dockerfile in the root directory of the project ${context.codebase.name}. "
+            context.script.error "There is no Dockerfile in the root directory of the project " +
+                    "${context.codebase.name}. "
         }
 
         context.logger.debug("Get version from pom.xml")
         String version = context.script.readMavenPom(file: "pom.xml").version
-        context.codebase.setImageTag("${version}.0")
+        context.codebase.setImageTag(version)
         context.logger.debug("Version from pom.xml: ${version}")
 
         if (context.platform.checkObjectExists(BUILD_CONFIG_API, context.codebase.buildConfigName)) {
             context.logger.info("Updating build config ${context.codebase.buildConfigName}")
-            context.platform.patch(BUILD_CONFIG_API, context.codebase.buildConfigName, "\"{\\\"spec\\\":{\\\"output\\\":" +
-                    "{\\\"to\\\":{\\\"name\\\":\\\"${context.codebase.imageUrl}\\\"}}}}\"")
+            context.platform.patch(BUILD_CONFIG_API, context.codebase.buildConfigName,
+                    "\"{\\\"spec\\\":{\\\"output\\\":" +
+                            "{\\\"to\\\":{\\\"name\\\":\\\"${context.codebase.imageUrl}\\\"}}}}\"")
         } else {
             context.logger.info("Creating build config ${context.codebase.buildConfigName}")
             context.script.sh(script: "oc new-build --name ${context.codebase.buildConfigName} " +
@@ -43,5 +49,13 @@ class BuildDockerfileImage {
         String tarArchive = "${context.codebase.name}.tar"
         context.script.sh(script: "tar -cf ${tarArchive} Dockerfile src pom.xml settings.xml; " +
                 "oc start-build ${context.codebase.buildConfigName} --from-archive=${tarArchive} --wait=true")
+
+    }
+
+    void updateDockerfile(dockerFileSourcePath, dockerProxyRegistry, NexusNamespace,
+                          dockerFileDestPath) {
+        String template = context.script.readFile(dockerFileSourcePath)
+        context.script.writeFile(file: dockerFileDestPath, text: TemplateRenderer.renderTemplate(template,
+                ["dockerProxyRegistry": dockerProxyRegistry, "globalNexusNamespace": NexusNamespace]))
     }
 }
