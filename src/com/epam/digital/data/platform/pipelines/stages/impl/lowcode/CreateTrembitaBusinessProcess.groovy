dfca488b8757fd2b6fee1fb37c6b58ebbf73b88c
Daniil Nedostup <daniil_nedostup@epam.com>
2021-11-26 09:49:41 +0200

    [EDP-DDM-7914] Added new update configMap and CR KeycloakAuthFlow steps, added KeycloakAuthFlow template
    Change-Id: I6586ed104bfc95fc71153c2b3066b628388b044c

--- src/com/epam/digital/data/platform/pipelines/stages/impl/lowcode/CreateTrembitaBusinessProcess.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/lowcode/CreateTrembitaBusinessProcess.groovy
@@ -20,6 +20,7 @@ import com.epam.digital.data.platform.pipelines.buildcontext.BuildContext
 import com.epam.digital.data.platform.pipelines.registry.RegulationType
 import com.epam.digital.data.platform.pipelines.stages.ProjectType
 import com.epam.digital.data.platform.pipelines.stages.Stage
+import com.epam.digital.data.platform.pipelines.tools.TemplateRenderer
 
 @Stage(name = "create-trembita-business-process", buildTool = ["any"], type = ProjectType.LIBRARY)
 class CreateTrembitaBusinessProcess {
@@ -39,6 +40,53 @@ class CreateTrembitaBusinessProcess {
         catch (any) {
             context.logger.error("Error during uploading trembita business process changes")
             context.stageFactory.runStage(context.RESTORE_STAGE, context)
+        }
+        if (context.registryRegulations.filesToDeploy.get(RegulationType.BUSINESS_PROCESS_TREMBITA)) {
+            try {
+                String trembitaConfFile = "${RegulationType.BUSINESS_PROCESS_TREMBITA.value}/configuration.yml"
+                String trembitaConfigmapKey = "trembita-configuration.yml"
+                String configmapName = "trembita-configuration"
+                String trembitaConfigmapYaml = "${context.script.sh(script: """awk '{printf "%s\\n", \$0}' \
+                        ${trembitaConfFile}""", returnStdout: true).replaceAll("\n", "\\\\n")}"
+                context.bpmsConfigMapsChanged["trembitaConfiguration"] = context.platform.patchConfigMapKey(configmapName,
+                        trembitaConfigmapKey, trembitaConfigmapYaml)
+                context.logger.info("Configmap trembita-configuration have been successfully updated")
+            }
+            catch (any) {
+                context.logger.error("Error during trembita-configuration configmap updating")
+            }
+            String cdPipelineName = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
+                    ".data.cdPipelineName")
+            String cdPipelineStageName = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
+                    ".data.cdPipelineStageName")
+            LinkedHashMap trembitaYaml = context.script.readYaml file: "bp-trembita/configuration.yml"
+            context.logger.info("Creating configuration from configuration.yml")
+            String template = context.script.libraryResource("${context.YAML_RESOURCES_RELATIVE_PATH}" +
+                    "/keycloakauthflow/citizen-portal-dso-citizen-auth-flow.yaml")
+            LinkedHashMap trembitaMapPath = trembitaYaml["trembita-exchange-gateway"]["registries"]["edr-registry"]
+            LinkedHashMap<String, String> binding = [
+                    "namespace"              : context.namespace,
+                    "trembitaUserId"         : trembitaMapPath["user-id"],
+                    "registryMemberClass"    : trembitaMapPath["service"]["member-class"],
+                    "clientXRoadInstance"    : trembitaMapPath["client"]["x-road-instance"],
+                    "registryMemberCode"     : trembitaMapPath["service"]["member-code"],
+                    "trembitaProtocolVersion": "${trembitaMapPath["protocol-version"]}",
+                    "registryXRoadInstance"  : trembitaMapPath["service"]["x-road-instance"],
+                    "clientSubsystemCode"    : trembitaMapPath["client"]["subsystem-code"],
+                    "clientMemberCode"       : trembitaMapPath["client"]["member-code"],
+                    "trembitaUrl"            : trembitaMapPath["trembita-url"],
+                    "registryToken"          : trembitaMapPath["authorization-token"],
+                    "registrySubsystemCode"  : trembitaMapPath["service"]["subsystem-code"],
+                    "clientMemberClass"      : trembitaMapPath["client"]["member-class"],
+                    "cdname"                 : "${cdPipelineName}-${cdPipelineStageName}"
+            ]
+            String destination = "trembita-configuration.yaml"
+            context.script.writeFile(file: destination, text: TemplateRenderer.renderTemplate(template, binding))
+            context.platform.apply(destination)
+            context.logger.info("Configuration from configuration.yml have been sucessfully created")
+
+        } else {
+            context.logger.info("Skip trembita configuration creation due to empty change list")
         }
     }
 }
