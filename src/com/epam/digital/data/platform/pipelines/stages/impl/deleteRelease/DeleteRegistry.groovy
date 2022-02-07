a2391f7b125bac082841763a73ab1e05239a8af9
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2022-02-07 15:50:20 +0000

    [EDP-DDM-11265] Add parallelization to Delete-release-registry-regulations pipe
    Change-Id: I110cead83cee72f93346564833cbb77614c91766

--- src/com/epam/digital/data/platform/pipelines/stages/impl/deleteRelease/DeleteRegistry.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/deleteRelease/DeleteRegistry.groovy
@@ -31,67 +31,74 @@ class DeleteRegistry {
     private String CLEANUP_PROCESS_HISTORY_SQL = "CleanupProcessHistory.sql"
 
     void run() {
-        try {
-            context.logger.info("Cleaning form provider DB")
-            context.platform.scale("deployment/${FormManagement.PROVIDER_DEPLOYMENT_NAME}", 0)
-            context.platform.podExec(FormManagement.PROVIDER_DB_POD, "mongo ${FormManagement.PROVIDER_DB_NAME} " +
-                    "--authenticationDatabase ${FormManagement.AUTH_DATABASE} " +
-                    "-u ${context.platform.getSecretValue(FormManagement.PROVIDER_DB_SECRET, "username")} " +
-                    "-p ${context.platform.getSecretValue(FormManagement.PROVIDER_DB_SECRET, "password")} " +
-                    "--eval \"db.dropDatabase()\"", FormManagement.PROVIDER_DB_CONTAINER)
-            context.platform.scale("deployment/${FormManagement.PROVIDER_DEPLOYMENT_NAME}", 1)
-
-            context.logger.info("Cleaning form modeler DB")
-            context.platform.scale("deployment/${FormManagement.MODELER_DEPLOYMENT_NAME}", 0)
-            context.platform.podExec(FormManagement.MODELER_DB_POD, "mongo ${FormManagement.MODELER_DB_NAME} " +
-                    "--authenticationDatabase ${FormManagement.AUTH_DATABASE} " +
-                    "-u ${context.platform.getSecretValue(FormManagement.MODELER_DB_SECRET, "username")} " +
-                    "-p ${context.platform.getSecretValue(FormManagement.MODELER_DB_SECRET, "password")} " +
-                    "--eval \"db.dropDatabase()\"", FormManagement.MODELER_DB_CONTAINER)
-            context.platform.scale("deployment/${FormManagement.MODELER_DEPLOYMENT_NAME}", 1)
-        } catch (any) {
-            context.logger.warn("There was an error during form management databases cleanup")
-        }
-
-        context.platform.scale("deployment/${BusinessProcMgmtSys.BPMS_DEPLOYMENT_NAME}", 0)
-        context.platform.scale("deployment/${BusinessProcMgmtSys.BP_ADMIN_PORTAL_DEPLOYMENT_NAME}", 0)
-        Map binding = ["OWNER_ROLE": context.citus.ownerRole]
-
-        context.logger.info("Copy DB scripts on citus master and replica")
-        String template = context.script.libraryResource("sql/${CLEANUP_REGISTRY_SQL}")
-        context.script.writeFile(file: "sql/${CLEANUP_REGISTRY_SQL}", text: TemplateRenderer.renderTemplate(template, binding))
-        String cleanupProcessHistorySqlResource = context.script.libraryResource("sql/${CLEANUP_PROCESS_HISTORY_SQL}")
-        context.script.writeFile(file: "sql/${CLEANUP_PROCESS_HISTORY_SQL}", text: cleanupProcessHistorySqlResource)
-        context.script.sh(script: "oc rsync sql/ ${context.citus.masterRepPod}:/tmp/")
-        context.script.sh(script: "oc rsync sql/ ${context.citus.masterPod}:/tmp/")
-
-        context.logger.info("Cleaning registry DB on replica")
-        context.citus.psqlScript(context.citus.masterRepPod, "/tmp/${CLEANUP_REGISTRY_SQL}", "-d ${context.registry.name}")
-
-        context.logger.info("Cleaning registry DB on master")
-        context.citus.psqlScript(context.citus.masterPod, "/tmp/${CLEANUP_REGISTRY_SQL}", "-d ${context.registry.name}")
-
-        context.logger.info("Cleaning process_history DB on master")
-        context.citus.psqlScript(context.citus.masterPod, "/tmp/${CLEANUP_PROCESS_HISTORY_SQL}")
-
-        context.platform.scale("deployment/${BusinessProcMgmtSys.BPMS_DEPLOYMENT_NAME}", 1)
-        context.platform.scale("deployment/${BusinessProcMgmtSys.BP_ADMIN_PORTAL_DEPLOYMENT_NAME}", 1)
-
-        context.logger.info("Removing Redash objects")
-        LinkedHashMap officerUsersYaml = context.script.readYaml file: "roles/officer.yml"
-        officerUsersYaml["roles"].each { String role ->
+        LinkedHashMap parallelDeletion = [:]
+        parallelDeletion["cleanFormManagementDB"] = {
             try {
-                context.citus.psqlCommand(context.citus.masterRepPod,
-                        "call p_delete_analytics_user('analytics_${role["name"]}')", context.registry.name)
-            }
-            catch (any) {
-                context.logger.info("Removing of Redash roles failed. Possibly, there are no Redash roles in database.")
+                context.logger.info("Cleaning form provider DB")
+                cleanFormManagementDB(FormManagement.PROVIDER_DEPLOYMENT_NAME, FormManagement.PROVIDER_DB_POD,
+                        FormManagement.PROVIDER_DB_NAME, FormManagement.PROVIDER_DB_SECRET, FormManagement.PROVIDER_DB_CONTAINER)
+
+                context.logger.info("Cleaning form modeler DB")
+                cleanFormManagementDB(FormManagement.MODELER_DEPLOYMENT_NAME, FormManagement.MODELER_DB_POD,
+                        FormManagement.MODELER_DB_NAME, FormManagement.MODELER_DB_SECRET, FormManagement.MODELER_DB_CONTAINER)
+
+            } catch (any) {
+                context.logger.warn("There was an error during form management databases cleanup")
             }
         }
-        context.platform.podExec("redash-viewer-postgresql-0",
-                "bash -c \'export PGPASSWORD=${context.platform.getSecretValue("redash-chart-postgresql","postgresql-password")}; psql redash -U redash -c \"DELETE FROM events WHERE id > 1; DELETE FROM users WHERE id > 1;\"\'", "")
-        context.redash.deleteRedashResource("${context.redash.viewerUrl}/api/data_sources",
-                context.redash.viewerApiKey)
-        context.redash.deleteRedashResource("${context.redash.viewerUrl}/api/groups", context.redash.viewerApiKey)
+        parallelDeletion["cleanCitusProviderDB"] = {
+            context.platform.scale("deployment/${BusinessProcMgmtSys.BPMS_DEPLOYMENT_NAME}", 0)
+            context.platform.scale("deployment/${BusinessProcMgmtSys.BP_ADMIN_PORTAL_DEPLOYMENT_NAME}", 0)
+            Map binding = ["OWNER_ROLE": context.citus.ownerRole]
+
+            context.logger.info("Copy DB scripts on citus master and replica")
+            String template = context.script.libraryResource("sql/${CLEANUP_REGISTRY_SQL}")
+            context.script.writeFile(file: "sql/${CLEANUP_REGISTRY_SQL}", text: TemplateRenderer.renderTemplate(template, binding))
+            String cleanupProcessHistorySqlResource = context.script.libraryResource("sql/${CLEANUP_PROCESS_HISTORY_SQL}")
+            context.script.writeFile(file: "sql/${CLEANUP_PROCESS_HISTORY_SQL}", text: cleanupProcessHistorySqlResource)
+            context.script.sh(script: "oc rsync sql/ ${context.citus.masterRepPod}:/tmp/")
+            context.script.sh(script: "oc rsync sql/ ${context.citus.masterPod}:/tmp/")
+
+            context.logger.info("Cleaning registry DB on replica")
+            context.citus.psqlScript(context.citus.masterRepPod, "/tmp/${CLEANUP_REGISTRY_SQL}", "-d ${context.registry.name}")
+
+            context.logger.info("Cleaning registry DB on master")
+            context.citus.psqlScript(context.citus.masterPod, "/tmp/${CLEANUP_REGISTRY_SQL}", "-d ${context.registry.name}")
+
+            context.logger.info("Cleaning process_history DB on master")
+            context.citus.psqlScript(context.citus.masterPod, "/tmp/${CLEANUP_PROCESS_HISTORY_SQL}")
+
+            context.platform.scale("deployment/${BusinessProcMgmtSys.BPMS_DEPLOYMENT_NAME}", 1)
+            context.platform.scale("deployment/${BusinessProcMgmtSys.BP_ADMIN_PORTAL_DEPLOYMENT_NAME}", 1)
+        }
+        parallelDeletion["removeRedashUsers"] = {
+            context.logger.info("Removing Redash objects")
+            LinkedHashMap officerUsersYaml = context.script.readYaml file: "roles/officer.yml"
+            officerUsersYaml["roles"].each { String role ->
+                try {
+                    context.citus.psqlCommand(context.citus.masterRepPod,
+                            "call p_delete_analytics_user('analytics_${role["name"]}')", context.registry.name)
+                }
+                catch (any) {
+                    context.logger.info("Removing of Redash roles failed. Possibly, there are no Redash roles in database.")
+                }
+            }
+            context.platform.podExec("redash-viewer-postgresql-0",
+                    "bash -c \'export PGPASSWORD=${context.platform.getSecretValue("redash-chart-postgresql", "postgresql-password")}; psql redash -U redash -c \"DELETE FROM events WHERE id > 1; DELETE FROM users WHERE id > 1;\"\'", "")
+            context.redash.deleteRedashResource("${context.redash.viewerUrl}/api/data_sources",
+                    context.redash.viewerApiKey)
+            context.redash.deleteRedashResource("${context.redash.viewerUrl}/api/groups", context.redash.viewerApiKey)
+        }
+        context.script.parallel(parallelDeletion)
+    }
+
+    void cleanFormManagementDB(String deploymentName, String dbPodName, String dbName, String dbSecretName, String dbContainerName) {
+        context.platform.scale("deployment/$deploymentName", 0)
+        context.platform.podExec(dbPodName, "mongo $dbName " +
+                "--authenticationDatabase ${FormManagement.AUTH_DATABASE} " +
+                "-u ${context.platform.getSecretValue(dbSecretName, "username")} " +
+                "-p ${context.platform.getSecretValue(dbSecretName, "password")} " +
+                "--eval \"db.dropDatabase()\"", dbContainerName)
+        context.platform.scale("deployment/$deploymentName", 1)
     }
 }
