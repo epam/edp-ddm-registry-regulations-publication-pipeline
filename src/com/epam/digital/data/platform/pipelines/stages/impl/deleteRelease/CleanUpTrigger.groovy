634bd7cbedd49c1979512ee3fa396e044c46dec4
Vasyl_Volkov <vasyl_volkov@epam.com>
2022-02-10 12:42:12 +0200

    [EDP-DDM-9721] Add timeout for cleanup job
    Change-Id: I907c834eaff5b23f00173ca5af03c194fb3beff8

--- src/com/epam/digital/data/platform/pipelines/stages/impl/deleteRelease/CleanUpTrigger.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/deleteRelease/CleanUpTrigger.groovy
@@ -27,53 +27,54 @@ class CleanUpTrigger {
     BuildContext context
 
     void run() {
+        context.script.timeout(unit: 'MINUTES', time: 10) {
+            LinkedHashMap parallelDeletion = [:]
+            parallelDeletion["removeDataServices"] = {
+                context.logger.info("Removing Data Services codebasebranches")
+                context.platform.deleteObject(Codebase.CODEBASEBRANCH_CR, "-l type=data-component")
+                context.platform.deleteObject(Codebase.CODEBASE_CR, "-l type=data-component")
+            }
+            parallelDeletion["removeRegistryRegulation"] = {
+                context.logger.info("Removing registry-regulations codebasebranch codebase CR")
+                context.platform.deleteObject(Codebase.CODEBASEBRANCH_CR, "${context.codebase.name}-${context.codebase.branch}")
+                context.platform.deleteObject(Codebase.CODEBASE_CR, context.codebase.name)
+                context.logger.info("Removing ${context.codebase.name} repo")
+                context.gitServer.deleteRepository(context.codebase.name)
+            }
+            context.script.parallel(parallelDeletion)
 
-        LinkedHashMap parallelDeletion = [:]
-        parallelDeletion["removeDataServices"] = {
-            context.logger.info("Removing Data Services codebasebranches")
-            context.platform.deleteObject(Codebase.CODEBASEBRANCH_CR, "-l type=data-component")
-            context.platform.deleteObject(Codebase.CODEBASE_CR, "-l type=data-component")
-        }
-        parallelDeletion["removeRegistryRegulation"] = {
-            context.logger.info("Removing registry-regulations codebasebranch codebase CR")
-            context.platform.deleteObject(Codebase.CODEBASEBRANCH_CR, "${context.codebase.name}-${context.codebase.branch}")
-            context.platform.deleteObject(Codebase.CODEBASE_CR, context.codebase.name)
-            context.logger.info("Removing ${context.codebase.name} repo")
-            context.gitServer.deleteRepository(context.codebase.name)
-        }
-        context.script.parallel(parallelDeletion)
+            String tmpGerritSecret = "repository-codebase-${context.codebase.name}-temp"
+            if (!context.platform.checkObjectExists("secret", tmpGerritSecret)) {
+                String edpGerritSecret = "edp-gerrit-ciuser"
+                context.platform.create("secret generic", tmpGerritSecret,
+                        "--from-literal='username=${context.platform.getSecretValue(edpGerritSecret, "username")}' " +
+                                "--from-literal='password=${context.platform.getSecretValue(edpGerritSecret, "password")}'")
+            }
 
-        String tmpGerritSecret = "repository-codebase-${context.codebase.name}-temp"
-        if (!context.platform.checkObjectExists("secret", tmpGerritSecret)) {
-            String edpGerritSecret = "edp-gerrit-ciuser"
-            context.platform.create("secret generic", tmpGerritSecret,
-                    "--from-literal='username=${context.platform.getSecretValue(edpGerritSecret, "username")}' " +
-                            "--from-literal='password=${context.platform.getSecretValue(edpGerritSecret, "password")}'")
-        }
-
-        context.logger.info("Creating ${context.codebase.name} codebase and codebasebranch CRs")
-        ["codebase", "codebasebranch"].each {
-            String emptyRepoUrl = context.codebase.sourceRepository.replaceAll(
-                    context.codebase.sourceRepository.substring(
-                            context.codebase.sourceRepository.lastIndexOf('/') + 1),
-                    "empty-template-registry-regulation")
-            String repoUrl = context.getParameterValue("RECREATE_EMPTY", "true").toBoolean()
-                    ? emptyRepoUrl : context.codebase.sourceRepository
-            Map binding = [
-                    "repoUrl"       : repoUrl,
-                    "codebaseName"  : context.codebase.name,
-                    "branch"        : context.codebase.branch,
-                    "defaultBranch" : context.codebase.branch,
-                    "version"       : context.registry.version,
-                    "buildTool"     : context.codebase.buildToolSpec,
-                    "jenkinsAgent"  : context.codebase.jenkinsAgent,
-                    "jobProvisioner": context.codebase.jobProvisioner
-            ]
-            String dest = "${context.codebase.name}-${it}.yaml"
-            String template = context.script.libraryResource("${context.YAML_RESOURCES_RELATIVE_PATH}" +
-                    "/${it}/${context.codebase.name}.yaml")
-            context.script.writeFile(file: dest, text: TemplateRenderer.renderTemplate(template, binding))
-            context.platform.apply(dest)
+            context.logger.info("Creating ${context.codebase.name} codebase and codebasebranch CRs")
+            ["codebase", "codebasebranch"].each {
+                String emptyRepoUrl = context.codebase.sourceRepository.replaceAll(
+                        context.codebase.sourceRepository.substring(
+                                context.codebase.sourceRepository.lastIndexOf('/') + 1),
+                        "empty-template-registry-regulation")
+                String repoUrl = context.getParameterValue("RECREATE_EMPTY", "true").toBoolean()
+                        ? emptyRepoUrl : context.codebase.sourceRepository
+                Map binding = [
+                        "repoUrl"       : repoUrl,
+                        "codebaseName"  : context.codebase.name,
+                        "branch"        : context.codebase.branch,
+                        "defaultBranch" : context.codebase.branch,
+                        "version"       : context.registry.version,
+                        "buildTool"     : context.codebase.buildToolSpec,
+                        "jenkinsAgent"  : context.codebase.jenkinsAgent,
+                        "jobProvisioner": context.codebase.jobProvisioner
+                ]
+                String dest = "${context.codebase.name}-${it}.yaml"
+                String template = context.script.libraryResource("${context.YAML_RESOURCES_RELATIVE_PATH}" +
+                        "/${it}/${context.codebase.name}.yaml")
+                context.script.writeFile(file: dest, text: TemplateRenderer.renderTemplate(template, binding))
+                context.platform.apply(dest)
+            }
         }
     }
 }
