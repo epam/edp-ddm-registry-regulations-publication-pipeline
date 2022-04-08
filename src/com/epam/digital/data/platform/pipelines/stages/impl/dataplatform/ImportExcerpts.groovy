43f2d4becafefb64ceb3846e739ce78f62d31551
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2022-04-08 15:05:39 +0000

    [EDP-DDM-12461] Add ceph bucket access parameters to report-publisher execution command
    Change-Id: I81e885194b833f42f690655ca401c783e0197190

--- src/com/epam/digital/data/platform/pipelines/stages/impl/dataplatform/ImportExcerpts.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/dataplatform/ImportExcerpts.groovy
@@ -18,21 +18,23 @@ package com.epam.digital.data.platform.pipelines.stages.impl.dataplatform
 
 import com.epam.digital.data.platform.pipelines.buildcontext.BuildContext
 import com.epam.digital.data.platform.pipelines.registry.RegulationType
+import com.epam.digital.data.platform.pipelines.registrycomponents.external.CephBucket
 import com.epam.digital.data.platform.pipelines.stages.ProjectType
 import com.epam.digital.data.platform.pipelines.stages.Stage
 
 @Stage(name = "import-excerpts", buildTool = ["any"], type = [ProjectType.LIBRARY])
 class ImportExcerpts {
     BuildContext context
-
     private final String EXCERPTS_IMPORTER_JAR = "/home/jenkins/report-publisher/report-publisher.jar"
     private final String EXCERPTS_DB_NAME = "excerpt"
 
     void run() {
         if (context.registryRegulations.filesToDeploy.get(RegulationType.EXCERPTS)) {
+            CephBucket excerptBucket = new CephBucket("excerpt-templates", context)
+            excerptBucket.init()
             try {
                 context.logger.info("Importing excerpts")
-                context.script.sh(script: "java -jar " +
+                context.script.sh(script: "set +x; java -jar " +
                         "-DREDASH_URL=${context.redash.viewerUrl} " +
                         "-DREDASH_API_KEY=${context.redash.viewerApiKey} " +
                         "-DPOSTGRES_PASSWORD=${context.citus.excerptExporterPass} " +
@@ -42,12 +44,16 @@ class ImportExcerpts {
                         "-DDB_PORT=${context.citus.CITUS_MASTER_PORT} " +
                         "-DPWD_ADMIN=${context.citus.analyticsAdminRolePass} " +
                         "-DPWD_AUDITOR=${context.citus.auditRolePass} " +
+                        "-DCEPH_BUCKET=${excerptBucket.cephBucketName} " +
+                        "-DCEPH_HTTP_ENDPOINT=${excerptBucket.cephHttpEndpoint} " +
+                        "-DCEPH_ACCESS_KEY=${excerptBucket.cephAccessKey} " +
+                        "-DCEPH_SECRET_KEY=${excerptBucket.cephSecretKey} " +
                         "${EXCERPTS_IMPORTER_JAR} " +
-                        "--excerpts " +
+                        "--excerpts --excerpts-docx" +
                         "${context.logLevel == "DEBUG" ? "1>&2" : ""}")
                 context.logger.info("Excerpts have been successfully imported")
             } catch (any) {
-                context.logger.warn("Excerpts import have been failed")
+                context.script.error("Excerpts import have been failed")
             }
         } else {
             context.logger.info("Skip ${RegulationType.EXCERPTS.value} import due to no changes")
