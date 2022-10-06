493a0278aef6107330357b46ccd8e0a2b91f716a
Dmytro Zinchenko <dmytro_zinchenko1@epam.com>
2022-10-06 10:38:37 +0300

    [EDP-DDM-16871] Add form data storage migration
    Change-Id: I649a192172ec7a079c1559bda3337d4bcef2564f

---
+++ src/com/epam/digital/data/platform/pipelines/registrycomponents/regular/Redis.groovy
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.digital.data.platform.pipelines.registrycomponents.regular
+
+class Redis {
+    public final static String REDIS_SECRET = "redis-auth"
+    public final static String REDIS_MASTER = "mymaster"
+    public final static String REDIS_NODES = "rfs-redis-sentinel:26379"
+}
