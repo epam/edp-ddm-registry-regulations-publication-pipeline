a6170e9caaf5b75dd56110ab6b10b45cc1709830
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2023-01-18 11:44:23 +0000

    [EDP-DDM-19050] Update external-systems-endpoint-configuration
    Change-Id: Id411cef33beb7ff9bf7afa812cffbed15388c8e2

--- src/com/epam/digital/data/platform/pipelines/stages/impl/lowcode/CreateTrembitaBusinessProcess.groovy
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/lowcode/CreateTrembitaBusinessProcess.groovy
@@ -20,7 +20,6 @@ import com.epam.digital.data.platform.pipelines.buildcontext.BuildContext
 import com.epam.digital.data.platform.pipelines.registry.RegulationType
 import com.epam.digital.data.platform.pipelines.stages.ProjectType
 import com.epam.digital.data.platform.pipelines.stages.Stage
-import com.epam.digital.data.platform.pipelines.tools.TemplateRenderer
 
 @Stage(name = "create-trembita-business-process", buildTool = ["any"], type = ProjectType.LIBRARY)
 class CreateTrembitaBusinessProcess {
@@ -44,78 +43,16 @@ class CreateTrembitaBusinessProcess {
         if (context.registryRegulations.filesToDeploy.get(RegulationType.BUSINESS_PROCESS_TREMBITA)) {
             try {
                 String trembitaConfFile = "${RegulationType.BUSINESS_PROCESS_TREMBITA.value}/configuration.yml"
-                String trembitaConfigmapKey = "trembita-configuration.yml"
-                String configmapName = "trembita-configuration"
+                String trembitaConfigmapKey = "external-systems-endpoint-configuration.yml"
+                String configmapName = "external-systems-endpoint-configuration"
                 String trembitaConfigmapYaml = "${context.script.sh(script: """awk '{printf "%s\\n", \$0}' \
                         ${trembitaConfFile}""", returnStdout: true).replaceAll("\n", "\\\\n")}"
                 context.bpmsConfigMapsChanged["trembitaConfiguration"] = context.platform.patchConfigMapKey(configmapName,
                         trembitaConfigmapKey, trembitaConfigmapYaml)
-                context.logger.info("Configmap trembita-configuration have been successfully updated")
+                context.logger.info("Configmap external-systems-endpoint-configuration have been successfully updated")
             }
             catch (any) {
-                context.logger.error("Error during trembita-configuration configmap updating")
-            }
-            String cdPipelineName = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
-                    ".data.cdPipelineName")
-            String cdPipelineStageName = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
-                    ".data.cdPipelineStageName")
-            String signWidgetUrl = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
-                    ".data.signWidgetUrl")
-            String digitalSignatureOpsUrl = context.platform.getJsonPathValue("configmap", "registry-pipeline-stage-name",
-                    ".data.digitalSignatureOpsUrl")
-            LinkedHashMap trembitaYaml = context.script.readYaml file: "bp-trembita/configuration.yml"
-            context.logger.info("Creating configuration from configuration.yml")
-            String template = context.script.libraryResource("${context.YAML_RESOURCES_RELATIVE_PATH}" +
-                    "/keycloakauthflow/citizen-portal-dso-citizen-auth-flow.yaml")
-            LinkedHashMap trembitaMapPath = trembitaYaml["trembita-exchange-gateway"]["registries"]["edr-registry"]
-            LinkedHashMap<String, String> binding = [
-                    "namespace"              : context.namespace,
-                    "trembitaUserId"         : trembitaMapPath["user-id"],
-                    "registryMemberClass"    : trembitaMapPath["service"]["member-class"],
-                    "clientXRoadInstance"    : trembitaMapPath["client"]["x-road-instance"],
-                    "registryMemberCode"     : trembitaMapPath["service"]["member-code"],
-                    "trembitaProtocolVersion": "${trembitaMapPath["protocol-version"]}",
-                    "registryXRoadInstance"  : trembitaMapPath["service"]["x-road-instance"],
-                    "clientSubsystemCode"    : trembitaMapPath["client"]["subsystem-code"],
-                    "clientMemberCode"       : trembitaMapPath["client"]["member-code"],
-                    "trembitaUrl"            : trembitaMapPath["trembita-url"],
-                    "registryToken"          : context.platform.getSecretValue("trembita-registries-secrets",
-                            "trembita\\.registries\\.edr-registry\\.auth\\.secret\\.token"),
-                    "registrySubsystemCode"  : trembitaMapPath["service"]["subsystem-code"],
-                    "clientMemberClass"      : trembitaMapPath["client"]["member-class"],
-                    "cdname"                 : "${cdPipelineName}-${cdPipelineStageName}",
-                    "dnsWildcard"            : context.dnsWildcard,
-                    "widgetUrl"              : "${signWidgetUrl}",
-                    "dsoUrl"                 : "${digitalSignatureOpsUrl}"
-            ]
-            String destination = "trembita-configuration.yaml"
-            context.script.writeFile(file: destination, text: TemplateRenderer.renderTemplate(template, binding))
-            context.platform.apply(destination)
-            context.logger.info("Configuration from configuration.yml have been sucessfully created")
-
-            if (trembitaYaml["external-systems"]) {
-                try {
-                    String serviceTemplate = context.script.libraryResource("${context.YAML_RESOURCES_RELATIVE_PATH}" +
-                            "/serviceentry/external-system-service-entry.yaml")
-                    String serviceDestination = "external-systems-configuration.yaml"
-                    trembitaYaml["external-systems"].each() { service ->
-                        ArrayList<String> serviceUrl = []
-                        serviceUrl.add("${service.getValue()["url"].replaceAll("http(s)?://|/.*", "")}")
-                        if (service.getValue()["auth"]["partner-token-auth-url"])
-                            serviceUrl.add(service.getValue()["auth"]["partner-token-auth-url"].replaceAll("http(s)?://|/.*", ""))
-                        LinkedHashMap<String, String> serviceBinding = [
-                                "serviceName": service.getKey(),
-                                "serviceUrl" : serviceUrl
-                        ]
-                        context.script.writeFile(file: serviceDestination, text: TemplateRenderer.renderTemplate(serviceTemplate, serviceBinding))
-                        context.platform.apply(serviceDestination)
-                        context.logger.info("Service entry for ${service.getKey()} have been successfully created")
-                    }
-                } catch (any) {
-                    context.logger.info("Error during external-systems service entry creating/updating")
-                }
-            } else {
-                context.logger.info("External-systems list is not found in configuration.yml")
+                context.logger.error("Error during external-systems-endpoint-configuration configmap updating")
             }
         } else {
             context.logger.info("Skip trembita configuration creation due to empty change list")
