434929cc356d9036b691f6af9b685638e4b0ca48
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2023-01-25 15:48:51 +0200

    [EDP-DDM-23095] Added code-review pipe for registry-regulations to check a new version candidate
    Change-Id: I0516e17edbfcc8e1edf625f01024b2fdb58043b2

---
+++ src/com/epam/digital/data/platform/pipelines/stages/impl/dataplatform/CheckVersionCandidateRegistryRegulations.groovy
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2023 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.digital.data.platform.pipelines.stages.impl.dataplatform
+
+import com.epam.digital.data.platform.pipelines.buildcontext.BuildContext
+import com.epam.digital.data.platform.pipelines.stages.ProjectType
+import com.epam.digital.data.platform.pipelines.stages.Stage
+
+@Stage(name = "check-version-candidate-registry-regulations", buildTool = ["any"], type = [ProjectType.LIBRARY])
+class CheckVersionCandidateRegistryRegulations {
+    BuildContext context
+
+    void run() {
+        context.logger.info("Code review pipeline")
+    }
+}
